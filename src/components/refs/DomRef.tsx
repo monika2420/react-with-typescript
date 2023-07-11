7da28655534d7865bcad3e799fe18f305d92e957
monika2420 <138773088+monika2420@users.noreply.github.com>
2023-07-11 17:26:02 +0530

    Add files via upload

---
+++ src/components/refs/DomRef.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+import { useRef, useEffect } from 'react'
+
+
+export const DomRef = () => {
+	const inputRef = useRef<HTMLInputElement>(null!)
+
+	useEffect(() => {
+		inputRef.current.focus()
+
+	}, [])
+
+	return (
+		<div>
+		  <input type='text' ref={inputRef} />
+		  </div>
+		  )
+}
+
