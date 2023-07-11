dc098b8bfa1e438571c8cbeecd65df19bbe073c4
monika2420 <138773088+monika2420@users.noreply.github.com>
2023-07-11 17:31:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ import { Status } from './components/Status'
 import { Button } from './components/Button'
 import { Input } from './components/Input'
 import { Container } from './components/Container'
+import { ThemeContextProvider } from './components/context/ThemeContext'
+import { Box } from './components/context/Box'
+import { User } from './components/context/User'
+import { UserContextProvider } from './components/context/UserContext'
+import { DomRef } from './components/refs/DomRef'
+import { MutableRef } from './components/refs/MutableRef'
+import { Counter } from './components/class/Counter'
+import { List } from './components/generics/List'
+  
+
+
+
 
 
 function App() {
@@ -30,22 +42,53 @@ function App() {
     last: 'Tesla'
   },
   ]
+
+
     return (
   <div className="App">
     <Greet name='vishwas' isloggedIn={false} />
     <Person name={personName} />
     <PersonList names={nameList} />
-     <Status status='loading' />
-      <Heading>Best Student Award</Heading>
-      <Oscar>
-        <Heading>Year Of The Student Award Goes to Nikola Tesla </Heading>
-      </Oscar>
-      <Button handleClick={(event, id) => {
-          console.log('Button clicked', event, id)
+    <Status status='loading' />
+    <Heading>Best Student Award</Heading>
+    <Oscar>
+      <Heading>Year Of The Student Award Goes to Nikola Tesla </Heading>
+    </Oscar>
+    <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id)
       }}
       />
       <Input value='' handleChange={event => console.log(event)} />
       <Container styles={{border: '1px solid black', padding: '1rem'}} />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
+      <DomRef />
+      <MutableRef />
+      <Counter message='The count value is ' />
+      
+
+      <List items={[
+      { 
+        id: 1,
+        first: 'Abdul',
+        last: 'Kalam',
+      },
+      {
+        id: 2,
+        first: 'Elon',
+        last: 'Musk',
+      },
+      {
+        id: 3,
+        first: 'Nikola',
+        last: 'Tesla',
+      },
+      ]}
+      onClick={(item) => console.log(item)} />
   </div>
   )
 }
